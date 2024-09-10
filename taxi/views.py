645a8705cbfe00d13100c4da042cf717b532c83c
Ihor Utkin <igorutkin2002@gmail.com>
2024-09-10 16:17:37 +0200

    Resolve

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +14,32 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    template_name = "taxi/manufacturer_list.html"
+    context_object_name = "manufacturer_list"
+    queryset = Manufacturer.objects.all()
+    ordering = "name"
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    context_object_name = "car_list"
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+class DriverListView(generic.ListView):
+    model = Driver
+    context_object_name = "driver_list"
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Car.objects.select_related("manufacturer")
